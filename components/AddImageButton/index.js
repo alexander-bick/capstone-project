1cb4bfc7ffc0159543b512d9014289a2f80316d3
Alexander Bick <AlexanderBick@me.com>
2023-08-02 12:40:28 +0200

    self close FormPage/index

---
+++ components/AddImageButton/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const AddImageButton = styled.button`
+  background-color: black;
+  padding: 0.8rem;
+  border-radius: 0.6rem;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+  border: none;
+  font-size: inherit;
+`;
